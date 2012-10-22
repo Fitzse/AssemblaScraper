f69aa5c1d219779c5e3ded7399f992da1d1a58b1
Sean Fitzgerald <sean.fitzgerald@devfacto.com>
2012-10-22 10:40:53 -0600

    Add specifications controller

--- AssemblaScaper/App_Start/RouteConfig.cs
+++ AssemblaScaper/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AssemblaScaper
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Specifications", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ AssemblaScaper/Controllers/SpecificationsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssemblaScaper.Controllers
+{
+    public class SpecificationsController : Controller
+    {
+        //
+        // GET: /Specifications/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
