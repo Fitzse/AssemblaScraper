a39f9c5c4433160b70b05eea268e749df3c2e42c
Sean Fitzgerald <sean.fitzgerald@devfacto.com>
2012-10-22 11:00:10 -0600

    Add space class to represent assembla space

---
+++ Assembla/Space.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Assembla
+{
+    [Serializable]
+    public class Space
+    {
+        [XmlElement("id")]
+        public int Id { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("wiki-name")]
+        public string WikiName { get; set; }
+
+        [XmlElement("wiki-format")]
+        public string WikiFormat { get; set; }
+    }
+}
