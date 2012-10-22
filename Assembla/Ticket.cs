e4cb876cc9c05dd49cbe1833f417fb887a83b714
Sean Fitzgerald <sean.fitzgerald@devfacto.com>
2012-10-22 10:53:43 -0600

    Add assembla project to handle API calls

---
+++ Assembla/Ticket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Assembla
+{
+    [Serializable]
+    public class Ticket
+    {
+        [XmlElement("id")]
+        public int Id { get; set; }
+
+        [XmlElement("number")]
+        public int Number { get; set; }
+
+        [XmlElement("summary")]
+        public string Summary { get; set; }
+
+        [XmlElement("reporter-id")]
+        public int ReporterId { get; set; }
+
+        [XmlElement("priority")]
+        public int Priority { get; set; }
+
+        [XmlElement("status")]
+        public int Status { get; set; }
+
+        [XmlElement("description")]
+        public string Description { get; set; }
+    }
+}
